714f6634f763c023c4db1b4e23405fc5c1da6695
Mohammad Azam <azamsharp@gmail.com>
2021-07-22 14:45:47 -0500

    changing port to PORT

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/hello', (req, res) => {
     res.json({'message': 'hello world'})
 })
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log('Server is running')
